5a6fadf9a9c74aae11e92719944bfbf068938619
bexaca <aleksandarberonja6@gmail.com>
2018-02-09 23:46:31 +0100

    volim te

--- process.php
+++ process.php
@@ -12,7 +12,7 @@
 	
 	$result=mysql_query("select* from users where username='$username' and password='$password'") or die("Failed to query database".mysql_error());
 	$row=mysql_fetch_array($result);
-	if($row['username']==$username && $row['password']==$password && $row['username']==admin){
+	if($row['username']==$username && $row['password']==$password && $row['uloga']==admin){
 	
 	header('Location:index.php');
 }
